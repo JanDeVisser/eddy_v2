2545d6aa2874d73cb1fc4b8facee2aad6bd2e91a
Jan de Visser <jan@de-visser.net>
2024-02-13 13:45:43 -0500

    Ugh this needs to go with the arena allocator removal commit

--- src/base/allocate.h
+++ src/base/allocate.h
@@ -12,42 +12,38 @@
 
 #ifdef STATIC_ALLOCATOR
 
-static Allocator *s_alloc = NULL;
+static Allocator s_alloc = { 0 };
 
-static Allocator *get_allocator()
+static Allocator get_allocator()
 {
-    if (!s_alloc) {
-#ifdef ALLOCATOR_ARENA_SZ
-        s_alloc = allocator_new_with_size(ALLOCATOR_ARENA_SZ);
-#else
+    if (!s_alloc.sentinel) {
         s_alloc = allocator_new();
-#endif
     }
     return s_alloc;
 }
 
 #else
 
-#define DECLARE_SHARED_ALLOCATOR(allocator)        \
-    extern Allocator *allocator##_get_allocator(); \
-    static Allocator *get_allocator()              \
-    {                                              \
-        return allocator##_get_allocator();        \
+#define DECLARE_SHARED_ALLOCATOR(allocator)       \
+    extern Allocator allocator##_get_allocator(); \
+    static Allocator get_allocator()              \
+    {                                             \
+        return allocator##_get_allocator();       \
     }
 
-#define SHARED_ALLOCATOR_IMPL(allocator)          \
-    static Allocator *s_alloc = NULL;             \
-    Allocator        *allocator##_get_allocator() \
-    {                                             \
-        if (!s_alloc) {                           \
-            s_alloc = allocator_new();            \
-        }                                         \
-        return s_alloc;                           \
+#define SHARED_ALLOCATOR_IMPL(allocator)         \
+    static Allocator s_alloc = { 0 };            \
+    Allocator        allocator##_get_allocator() \
+    {                                            \
+        if (!s_alloc.sentinel) {                 \
+            s_alloc = allocator_new();           \
+        }                                        \
+        return s_alloc;                          \
     }
 
 #endif /* STATIC_ALLOCATOR */
 
-static Allocator *get_allocator();
+static Allocator get_allocator();
 
 static inline void *allocate(size_t size)
 {
@@ -57,23 +53,6 @@ static inline void *allocate(size_t size)
 static inline void *array_allocate(size_t size_of_elem, size_t num_of_elems)
 {
     return allocator_allocate_array(get_allocator(), size_of_elem, num_of_elems);
-}
-
-static inline void save_allocator()
-{
-    allocator_save(get_allocator());
-}
-
-static inline void release_allocator()
-{
-    assert(get_allocator());
-    allocator_release(get_allocator());
-}
-
-static inline void reset_allocator()
-{
-    assert(get_allocator());
-    allocator_reset(get_allocator());
 }
 
 #define allocate_new(t) allocate(sizeof(t))
